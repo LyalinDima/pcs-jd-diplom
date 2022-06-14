e0ef16e8a5f40e8bb8f788286d2b86020001d864
LyalinDima <lyalindim@gmail.com>
2022-06-14 21:07:34 +0500

    First commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,43 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> answers = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        for (File pdfFile : Objects.requireNonNull(pdfsDir.listFiles((f, name) -> name.endsWith(".pdf")))) {
+            PdfDocument doc = new PdfDocument(new PdfReader(pdfFile));
+            for (int i = 1; i < doc.getNumberOfPages() + 1; i++) {
+                PdfPage page = doc.getPage(i);
+                String text = PdfTextExtractor.getTextFromPage(page);
+                String[] words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>();
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    freqs.put(word.toLowerCase(), freqs.getOrDefault(word.toLowerCase(), 0) + 1);
+                }
+                for (String word : freqs.keySet()) {
+                    List<PageEntry> pageEntries = answers.getOrDefault(word, new ArrayList<>());
+                    pageEntries.add(new PageEntry(pdfFile.getName(), i, freqs.get(word)));
+                    answers.put(word, pageEntries);
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> answer = answers.get(word);
+        if (!answer.isEmpty()) {
+            Collections.sort(answer);
+        }
+        return answer;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,38 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
 
 public class Main {
+    public static final int SERVER_PORT = 8989;
+    public static final String SERVER_HOST = "localhost";
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
-
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    String word = in.readLine();
+                    List<PageEntry> answer = engine.search(word);
+                    GsonBuilder builder = new GsonBuilder();
+                    Gson gson = builder.create();
+                    Type listType = new TypeToken<List<PageEntry>>() {
+                    }.getType();
+                    String result = gson.toJson(answer, listType);
+                    out.println(result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
